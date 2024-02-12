305fb3bc3cdb9831aa78476aaede2482a9c926be
Tev Nicolas <tevnicolas@alum.calarts.edu>
2024-02-12 23:35:34 +0000

    Completed dom-manipulation exercise.

---
+++ dom-manipulation/main.js
@@ -0,0 +1,24 @@
+'use strict';
+let counter = 0;
+const $hotButton = document.querySelector('.hot-button');
+const $clickCount = document.querySelector('.click-count');
+if (!$hotButton) throw new Error('The $hotButton query failed');
+$hotButton.addEventListener('click', () => {
+  counter++;
+  if (!$hotButton || !$clickCount)
+    throw new Error('The $hotButton or $clickCount query failed');
+  $clickCount.textContent = 'Clicks: ' + String(counter);
+  if (counter < 4) {
+    $hotButton.className = 'hot-button cold';
+  } else if (counter < 7) {
+    $hotButton.className = 'hot-button cool';
+  } else if (counter < 10) {
+    $hotButton.className = 'hot-button tepid';
+  } else if (counter < 13) {
+    $hotButton.className = 'hot-button warm';
+  } else if (counter < 16) {
+    $hotButton.className = 'hot-button hot';
+  } else {
+    $hotButton.className = 'hot-button nuclear';
+  }
+});
