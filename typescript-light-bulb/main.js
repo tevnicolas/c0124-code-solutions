8d474a5c1d4c0a9c26e19df32c4336268fd1dc89
Tev Nicolas <tevnicolas@alum.calarts.edu>
2024-02-14 18:48:30 +0000

    Completed typescript-light-bulb challenge.

---
+++ typescript-light-bulb/main.js
@@ -0,0 +1,15 @@
+'use strict';
+const $circle = document.querySelector('#circle');
+const $container = document.querySelector('#container');
+if (!$circle) throw new Error('missing #circle div element');
+if (!$container) throw new Error('missing #container div element');
+$circle.addEventListener('click', (event) => {
+  const $eventTarget = event.target;
+  if ($eventTarget.className === 'light') {
+    $circle.setAttribute('class', 'dark');
+    $container.setAttribute('class', 'black');
+  } else {
+    $circle.setAttribute('class', 'light');
+    $container.setAttribute('class', 'white');
+  }
+});
