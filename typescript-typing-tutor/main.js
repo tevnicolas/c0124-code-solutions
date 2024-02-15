3e626b5261bd4caa68d4b6092e74d32195cecbd2
Tev Nicolas <tevnicolas@alum.calarts.edu>
2024-02-15 02:56:40 +0000

    Completed typescript-typing-tutor challenge.

---
+++ typescript-typing-tutor/main.js
@@ -0,0 +1,16 @@
+'use strict';
+const $span = document.querySelectorAll('#phrase > span');
+const $input = document.querySelector('body');
+if (!$span) throw new Error('Error! No letters available.');
+if (!$input) throw new Error('Error, no input available.');
+$span[0].setAttribute('class', 'underlined');
+let currentIndex = 0;
+$input.addEventListener('keydown', (event) => {
+  if (event.key === $span[currentIndex].textContent) {
+    $span[currentIndex].setAttribute('class', 'green');
+    $span[currentIndex + 1].setAttribute('class', 'underlined');
+    currentIndex++;
+  } else {
+    $span[currentIndex].setAttribute('class', 'red');
+  }
+});
