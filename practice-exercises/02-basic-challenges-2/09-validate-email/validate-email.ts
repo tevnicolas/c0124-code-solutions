9efe5841f8e7cd6f17db0a04b1e581fff5a4224d
Tev Nicolas <tevnicolas@alum.calarts.edu>
2024-02-26 21:59:28 +0000

    Little changes

--- practice-exercises/02-basic-challenges-2/09-validate-email/validate-email.ts
+++ practice-exercises/02-basic-challenges-2/09-validate-email/validate-email.ts
@@ -21,3 +21,8 @@ export function validateEmail(email: string): boolean {
     condition8
   );
 }
+
+// export function validateEmail(email: string): boolean {
+//   const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+//   return emailRegex.test(email);
+// }
--- practice-exercises/03-high-order-array-methods/01-simple-examples/simple-examples.ts
+++ practice-exercises/03-high-order-array-methods/01-simple-examples/simple-examples.ts
@@ -3,11 +3,15 @@ const numbers = [1, 2, 3, 4, 5];
 /**
  * map: Transforms array elements with a provided function, creating a new array.
  */
-
+const newNumbers1 = numbers.map((num) => {
+  return num * 2;
+});
 /**
  * filter: Creates a new array with elements that satisfy a specified condition.
  */
-
+const newNumbers2 = numbers.filter((num) => {
+  return num % 2 === 0;
+});
 /**
  * reduce: Accumulates array elements into a single value using a provided function.
  */
