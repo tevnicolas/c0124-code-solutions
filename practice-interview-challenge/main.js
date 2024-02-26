93ec9076b3f971e49cbf5f8e2953828bddba7689
Tev Nicolas <tevnicolas@alum.calarts.edu>
2024-02-26 17:40:16 +0000

    Completed practice-interview-problem.

---
+++ practice-interview-challenge/main.js
@@ -0,0 +1,28 @@
+'use strict';
+function getRangeReport(start, end) {
+  const average = (start + end) / 2;
+  const odds = [];
+  const evens = [];
+  const range = [];
+  for (let i = start; i <= end; i++) {
+    range.push(i);
+    if (i % 2 === 0) {
+      evens.push(i);
+    } else {
+      odds.push(i);
+    }
+  }
+  const total = range.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0);
+  const newObj = {
+    total,
+    odds,
+    evens,
+    range,
+    average,
+  };
+  return newObj;
+}
+console.log(getRangeReport(1, 10)); // -> {average: 5.5, evens:[2,4,6,8,10], odds:[1,3,5,7,9], range:[1,2,3,4,5,6,7,8,9,10], total: 55 }
+console.log(getRangeReport(1, 5)); // -> {average: 3, evens:[2,4], odds:[1,3,5], range:[1,2,3,4,5], total: 15 }
