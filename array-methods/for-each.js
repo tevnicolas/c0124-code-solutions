d2d8afea762db3359daebba9c03fdcccf1cf9dad
Tev Nicolas <tevnicolas@alum.calarts.edu>
2024-02-28 00:38:19 +0000

    Completed array-methods exercise.

---
+++ array-methods/for-each.js
@@ -0,0 +1,8 @@
+'use strict';
+const values = [10, 1, 22, 23, 41, 5, 18, 7, 80, 9];
+values.forEach((num) => {
+  console.log(num);
+});
+values.forEach((num, index, array) => {
+  console.log(array[array.length - 1 - index]);
+});
