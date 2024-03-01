febda238166bdd91a74d04fd520692362139d8e3
Tev Nicolas <tevnicolas@alum.calarts.edu>
2024-03-01 19:04:28 +0000

    Completed fetch exercise.

---
+++ fetch/main.ts
@@ -0,0 +1,12 @@
+async function getRequest(): Promise<void> {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    if (!response.ok) throw new Error('Yikes Error Code: ' + response.status);
+    const array = await response.json();
+    console.log('Array', array);
+  } catch (error) {
+    console.log('ThrowError', error);
+  }
+}
+
+getRequest();
