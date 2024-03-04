de06cfe9286f217bb237c260ae088d9d4fd90b78
Tev Nicolas <tevnicolas@alum.calarts.edu>
2024-03-04 18:55:10 +0000

    Little updates

--- practice-exercises/03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.ts
+++ practice-exercises/03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.ts
@@ -1,22 +1,22 @@
 export function highestScoringWord(str: string): string {
   const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-  const alpArr: string[] = [];
-  for (const letter of alphabet) {
-    alpArr.push(letter);
-  }
+  const alpArr = alphabet.split('');
 
   const strArr = str.split(' ');
-  const winner = strArr.reduce((acc: Record<string, number>, curr) => {
-    let points = 0;
-    const newObj: Record<string, number> = {};
-    for (const letter of curr) {
-      points += alpArr.indexOf(letter);
-    }
-    if (points > Object.values(acc)[0] || Object.keys(acc).length === 0) {
-      newObj[curr] = points;
-    }
-    return newObj;
-  }, {});
-
-  return Object.keys(winner)[0];
+  const winner = strArr.reduce(
+    (acc: Record<string, number> | string, curr, i, arr) => {
+      let points = 0;
+      const newObj: Record<string, number> = {};
+      for (const letter of curr) {
+        points += alpArr.indexOf(letter);
+      }
+      if (points > Object.values(acc)[0] || Object.keys(acc).length === 0) {
+        newObj[curr] = points;
+        return i + 1 !== arr.length ? newObj : curr;
+      }
+      return i + 1 !== arr.length ? acc : Object.keys(acc)[0];
+    },
+    {}
+  );
+  return winner as string;
 }
