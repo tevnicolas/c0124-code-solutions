a339675f307ce0d45c644aa236ff269972bf3188
Tev Nicolas <tevnicolas@alum.calarts.edu>
2024-03-20 19:05:52 +0000

    Refactored code

--- react-hot-button/my-app/src/App.tsx
+++ react-hot-button/my-app/src/App.tsx
@@ -1,8 +1,34 @@
 import './App.css';
 import { HotButton } from './HotButton';
+import { useState } from 'react';
 
 function App() {
-  return <HotButton label="Hot Button" />;
+  const [count, setCount] = useState(0);
+  function handleClick(): void {
+    setCount(count + 1);
+  }
+  let color = 'black';
+  if (count < 3) {
+    color = 'black';
+  } else if (count < 6) {
+    color = 'navy';
+  } else if (count < 9) {
+    color = 'lavender';
+  } else if (count < 12) {
+    color = 'red';
+  } else if (count < 15) {
+    color = 'orange';
+  } else if (count < 18) {
+    color = 'yellow';
+  } else {
+    color = 'white';
+  }
+  return (
+    <HotButton
+      label={'Hot Button ' + count}
+      onHotButtonClick={handleClick}
+      color={color}></HotButton>
+  );
 }
 
 export default App;
--- react-hot-button/my-app/src/HotButton.tsx
+++ react-hot-button/my-app/src/HotButton.tsx
@@ -1,34 +1,13 @@
-import { useState } from 'react';
-
 type Props = {
   label: string;
+  color: string;
+  onHotButtonClick: () => void;
 };
 
-export function HotButton({ label }: Props) {
-  const [count, setCount] = useState(0);
-  const [color, setColor] = useState('black');
-  function handleClick(): void {
-    setCount(count + 1);
-    if (count + 1 < 3) {
-      setColor('black');
-    } else if (count + 1 < 6) {
-      setColor('navy');
-    } else if (count + 1 < 9) {
-      setColor('lavender');
-    } else if (count + 1 < 12) {
-      setColor('red');
-    } else if (count + 1 < 15) {
-      setColor('orange');
-    } else if (count + 1 < 18) {
-      setColor('yellow');
-    } else if (count + 1 > 17) {
-      setColor('white');
-    }
-  }
-
+export function HotButton({ label, color, onHotButtonClick }: Props) {
   return (
-    <button onClick={handleClick} type="button" className={color}>
-      {label} {count}
+    <button onClick={onHotButtonClick} type="button" className={color}>
+      {label}
     </button>
   );
 }
