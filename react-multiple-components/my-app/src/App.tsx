091692a4f39a9ffedead1c28286a041ae4455ba9
Tev Nicolas <tevnicolas@alum.calarts.edu>
2024-03-22 23:56:05 +0000

    Phase 1 completed.

---
+++ react-multiple-components/my-app/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+import { RotatingBanner } from './RotatingBanner';
+
+const items = [
+  'Aardvark',
+  'Bengal',
+  'Caterpillar',
+  'Dromedary',
+  'Elephant',
+  'Ferret',
+];
+
+function App() {
+  return <RotatingBanner items={items} />;
+}
+
+export default App;
---
+++ react-multiple-components/my-app/src/RotatingBanner.tsx
@@ -0,0 +1,70 @@
+// import { useState } from 'react';
+import './RotatingBanner.css';
+
+type RBProps = {
+  items: string[];
+};
+
+export function RotatingBanner({ items }: RBProps) {
+  // const [index, setIndex] = useState(0);
+  const numOfIndicators = items.length;
+
+  return (
+    <>
+      <Banner name={items[0]} />
+      <PreviousButton />
+      <Indicators number={numOfIndicators} />
+      <NextButton />
+    </>
+  );
+}
+
+type BProps = {
+  name: string;
+};
+
+function Banner({ name }: BProps) {
+  return (
+    <div>
+      <p>{name}</p>
+    </div>
+  );
+}
+
+// type PrevNextProps = {};
+
+function PreviousButton() {
+  return (
+    <div className="flexxer">
+      <div className="box">
+        <p>Prev</p>
+      </div>
+    </div>
+  );
+}
+
+type IProps = {
+  number: number;
+};
+
+function Indicators({ number }: IProps) {
+  const indicatorDivs = [];
+  for (let i = 0; i < number; i++) {
+    indicatorDivs.push(
+      <div className="box" key={i}>
+        <p>{i}</p>
+      </div>
+    );
+  }
+  return <div className="flexxer">{indicatorDivs}</div>;
+}
+
+function NextButton() {
+  return (
+    <div className="flexxer">
+      <div className="box">
+        <p>Next</p>
+      </div>
+    </div>
+  );
+}
